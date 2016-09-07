f628a2a0d2d4d07393c8b67ac9f798d21b0b20d1
miyaviee <merveilles1998318@yahoo.co.jp>
2016-09-07 19:26:48 +0900

    fix response header

--- app.py
+++ app.py
@@ -26,7 +26,9 @@ def index(message = None):
         parsed = brain.parse(message)
         res = brain.forget(parsed)
 
-    return jsonify(res)
+    response = jsonify(res)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
