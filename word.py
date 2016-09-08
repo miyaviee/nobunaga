aebcae25f97ee94996eacd308947ae1515001043
miyaviee <merveilles1998318@yahoo.co.jp>
2016-09-09 01:00:00 +0900

    update parse

--- word.py
+++ word.py
@@ -15,127 +15,32 @@ class Analysis(object):
         self.t = Tokenizer()
 
     def parse(self, word):
-        tmp = ''
-        parsed = {}
+        word = re.sub(u'(織田)?信長は', '', word)
+        parsed = {'word': word}
         for token in self.t.tokenize(word):
-            if re.search(u'代名詞', token.part_of_speech):
-                if re.search(u'ダレ', token.reading):
-                    if re.search(token.surface + u'(が|に)', word):
-                        parsed['subject_type'] = 'person'
-                        continue
-
-                    if re.search(token.surface + u'(を|は)', word):
-                        parsed['object_type'] = 'person'
-                        continue
-
-                if re.search(u'ドコ', token.reading):
-                    if 'object' in parsed:
-                        del parsed['object']
-                    parsed['object_type'] = 'place'
-                    continue
-
-                if re.search(u'ソコ', token.reading):
-                    if 'object' in parsed:
-                        del parsed['object']
-                    parsed['object_type'] = 'place'
-                    continue
-
-
-            if re.search(u'固有名詞', token.part_of_speech):
-                if re.search(u'人名', token.part_of_speech):
-                    tmp += token.surface
-                    if re.search(token.surface + u'(が|に)', word):
-                        if 'subject' in parsed:
-                            parsed['object'] = tmp
-                            parsed['object_type'] = 'person'
-                            continue
-
-                        parsed['subject'] = tmp
-                        parsed['subject_type'] = 'person'
-
-                    if re.search(token.surface + u'(を|は)', word):
-                        parsed['object'] = tmp
-                        parsed['object_type'] = 'person'
-                    continue
-
-                if re.search(u'地域', token.part_of_speech):
-                    if re.search(token.surface + u'(に|で|へ)', word):
-                        parsed['object'] = token.surface
-                        parsed['object_type'] = 'place'
-                    continue
-
-                parsed['object'] = token.surface
-                parsed['object_type'] = 'etc'
+            print(token)
+            if re.search(u'終助詞', token.part_of_speech):
                 continue
 
-            if re.search(u'名詞', token.part_of_speech):
-                if re.search(u'ナニ', token.reading):
-                    if re.search(token.surface + u'(で)', token.part_of_speech):
-                        parsed['object_type'] = 'etc'
-                        tmp = ''
-                    continue
-
-                if re.search(u'ナン', token.reading):
-                    if re.search(token.surface + u'(で)', token.part_of_speech):
-                        parsed['object_type'] = 'etc'
-                        tmp = ''
-                    continue
-
-                if re.search(u'名詞,接尾', token.part_of_speech):
-                    if token.reading == 'ネン':
-                        if re.search(u'^\d+$', tmp):
-                            tmp += token.surface
-                            parsed['object'] = tmp
-                        parsed['object_type'] = 'year'
-
-                    if token.reading == 'ツキ':
-                        if re.search(u'^\d+$', tmp):
-                            tmp += token.surface
-                            parsed['object'] = tmp
-                        parsed['object_type'] = 'month'
-
-                    if token.reading == '日':
-                        if re.search(u'^\d+$', tmp):
-                            tmp += token.surface
-                            parsed['object'] = tmp
-                        parsed['object_type'] = 'day'
-
-                if re.search(u'形容動詞語幹', token.part_of_speech):
-                    parsed['verb'] = token.base_form
-                    break
-
-                tmp = token.surface
+            if re.search(u'代名詞|副詞', token.part_of_speech):
+                if 'word' in parsed:
+                    del parsed['word']
                 continue
 
-            if re.search(u'動詞', token.part_of_speech):
-                parsed['verb'] = token.base_form
-                break
+            if re.search(u'ナニ|ナン', token.reading):
+                if 'word' in parsed:
+                    del parsed['word']
+                continue
 
-            tmp = ''
-
-        if 'subject_type' not in parsed:
-            parsed['subject'] = '織田信長'
-            parsed['subject_type'] = 'person'
-
-        if 'object_type' not in parsed:
-            parsed['object'] = '織田信長'
-            parsed['object_type'] = 'person'
+            parsed[token.part_of_speech] = token.surface
 
         return parsed
 
     def learn(self, parsed):
-        data = {}
         res = {'error': False}
-
-        if 'subject' not in parsed or 'object' not in parsed or 'verb' not in parsed:
+        if 'word' not in parsed:
             res['message'] = u'何が言いたいのだ'
             return res
-
-        if re.search(u'信長', parsed['subject']):
-            parsed['subject'] = '織田信長'
-
-        if re.search(u'信長', parsed['object']):
-            parsed['object'] = '織田信長'
 
         know = self.db.word.find_one(parsed)
         if know is not None:
@@ -150,32 +55,36 @@ class Analysis(object):
     def answer(self, parsed):
         res = {'error': False}
 
-        if 'subject_type' not in parsed and 'object_type' not in parsed:
-            res['message'] = u'何が言いたいのだ'
-            return res
-
-        if 'subject' not in parsed:
-            key = 'subject'
-
-        if 'object' not in parsed:
-            key = 'object'
-
-        if 'verb' not in parsed:
-            key = 'verb'
-
         data = self.db.word.find_one(parsed)
         if data is None:
             res['error'] = True
             res['message'] = u'うっ！頭が・・・思い出せぬ・・・'
             return res
 
+        del data['_id']
+
+        value = []
+        for v in data.values():
+            if v not in parsed.values():
+                value.append(v)
+
         try:
-            _var = key
-        except NameError:
-            res['message'] = u'よく知っておるな'
+            _var = value
+        except:
+            res['message'] = 'よく知っておるな'
             return res
 
-        res['message'] = u'それは・・・%sだ' % data[key]
+        if len(value) == 2:
+            value.remove(data['word'])
+            word = ''.join(value)
+            if word.isdigit():
+                res['message'] = data['word']
+                return res
+
+            res['message'] = u'それは・・・%sだ' % word
+            return res
+
+        res['message'] = data['word']
         return res
 
     def forget(self, parsed):
