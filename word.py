725cbb67ed3c87f5faf9978f9ea7180a9d90575c
miyaviee <merveilles1998318@yahoo.co.jp>
2016-09-09 22:37:34 +0900

    add know message

--- word.py
+++ word.py
@@ -6,7 +6,6 @@ import re
 import os
 
 class Analysis(object):
-
     def __init__(self):
         if os.environ.get('MONGODB_URI'):
             self.db = MongoClient(os.environ.get('MONGODB_URI')).heroku_8n89r7k9
@@ -15,78 +14,75 @@ class Analysis(object):
         self.t = Tokenizer()
 
     def parse(self, word):
-        parsed = {'word': word}
-        for token in self.t.tokenize(word):
-            if re.search(u'終助詞|記号', token.part_of_speech):
-                continue
+        self.word = word
+        return self.t.tokenize(word)
 
-            if re.search(u'代名詞|副詞|連体詞|副助詞', token.part_of_speech):
-                if 'word' in parsed:
-                    del parsed['word']
+    def learn(self, tokens):
+        error = True
+        for token in tokens:
+            if re.search(u'代名詞', token.part_of_speech):
+                return {
+                    'error': error,
+                    'message': u'何が言いたいのだ',
+                }
+
+            data = {
+                'keyword': token.surface,
+                'type': token.part_of_speech,
+                'origin': self.word,
+            }
+
+            result = self.db.word.find_one(data)
+            if result is None:
+                error = False
+                self.db.word.insert_one(data)
+
+        if error:
+            return {
+                'error': error,
+                'message': u'知っておるわ',
+            }
+
+        return {
+            'error': False,
+            'message': u'思い出したぞ・・・',
+        }
+
+    def answer(self, tokens):
+        query = []
+        error = True
+        for token in tokens:
+            if re.search(u'代名詞', token.part_of_speech):
+                error = False
                 continue
 
             if re.search(u'ナニ|ナン', token.reading):
-                if 'word' in parsed:
-                    del parsed['word']
+                error = False
                 continue
 
-            parsed[token.surface] = token.part_of_speech
+            data = {
+                'keyword': token.surface,
+                'type': token.part_of_speech,
+            }
 
-        return parsed
+            query.append(data)
 
-    def learn(self, parsed):
-        res = {'error': False}
-        if 'word' not in parsed:
-            res['message'] = u'何が言いたいのだ'
-            return res
+        if error:
+            return {
+                'error': error,
+                'message': u'何が言いたいのだ',
+            }
 
-        know = self.db.word.find_one(parsed)
-        if know is not None:
-            res['message'] = u'知っておるわ'
-            return res
+        result = self.db.word.find({'$or': query}).distinct('origin')
+        if result is None:
+            return {
+                'error': True,
+                'message': u'うっ！頭が・・・思い出せぬ・・・',
+            }
 
-        self.db.word.insert_one(parsed)
+        return {
+            'error': False,
+            'message': result.pop()
+        }
 
-        res['message'] = u'思い出したぞ・・・'
-        return res
 
-    def answer(self, parsed):
-        res = {'error': False}
-
-        if 'word' in parsed:
-            res['message'] = u'何が言いたいのだ'
-            return res
-
-        data = self.db.word.find_one(parsed)
-        if data is None:
-            res['error'] = True
-            res['message'] = u'うっ！頭が・・・思い出せぬ・・・'
-            return res
-
-        del data['_id']
-
-        value = []
-        for v in data.keys():
-            if v not in parsed.keys():
-                value.append(v)
-
-        if len(value) == 0:
-            res['message'] = 'よく知っておるな'
-            return res
-
-        if len(value) == 2:
-            value.remove('word')
-            word = ''.join(value)
-            if word.isdigit() or not word:
-                res['message'] = data['word']
-                return res
-
-            res['message'] = u'それは・・・%sだ' % word
-            return res
-
-        res['message'] = data['word']
-        return res
-
-    def forget(self, parsed):
-        res = {'error': False}
-        return res
